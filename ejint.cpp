d02436eea63e2191d81d7f7e51e5d40e407a4088
jdev2443-cmd <jdev2443@gmail.com>
2025-09-13 09:57:08 -0500

    ejint

---
+++ ejint.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main(){
+    int num1, num2, suma;
+
+    std::cout<<"ingrese el valor 1 ";
+    std::cin>>num1;
+
+    std::cout<<"ingrse el valor 2 ";
+    std::cin>>num2;
+    
+    suma = num1 + num2;
+    std::cout <<"la suma es "<< suma;
+    return 0;
+
+}
