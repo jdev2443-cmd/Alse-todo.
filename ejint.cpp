52ffba33a7e653540096d9a8b3b62a374cb30b2f
jdev2443-cmd <jdev2443@gmail.com>
2025-09-14 21:06:40 -0500

    ejint

--- ejint.cpp
+++ ejint.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main(){
-    int num1, num2, suma;
+    int num1, num2, suma, resta, multiplicacion, division;
 
     std::cout<<"ingrese el valor 1 ";
     std::cin>>num1;
@@ -8,8 +8,19 @@ int main(){
     std::cout<<"ingrse el valor 2 ";
     std::cin>>num2;
     
+    
     suma = num1 + num2;
     std::cout <<"la suma es "<< suma;
+
+    resta = num1 - num2;
+    std::cout<<"la resta de los numeros es "<< resta;
+
+    multiplicacion = num1 * num2;
+    std::cout<<"la multiplicacion de los numeros es "<<multiplicacion;
+
+    division = num1 / num2;
+    std::cout<<"la division de los numeros es "<< division;
+ 
     return 0;
 
 }
